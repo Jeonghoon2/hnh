3f95e10455291d2881cd6d2170ce5ce7b20e9313
Jeonghoon2 <ukidd12@naver.com>
2024-09-26 11:04:01 +0900

    fix hotdog

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Union
+from fastapi import pi
 import random
 
 app = FastAPI()
@@ -8,5 +10,5 @@ ans = ["This is Hot-🐶", "This is not Hot-🐶"]
 
 @app.get("/predict")
 def predict():
-
+    model = pipline("image-classification", model="julien-c/hotdog-not-hotdog")
     return {"message": random.choice(ans)}
